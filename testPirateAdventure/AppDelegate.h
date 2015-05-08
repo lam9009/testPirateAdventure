230e3f9ba9cd7240a8d896d1c8f5e183a82af130
Alfred <Alfred@Alfreds-MacBook-Pro.local>
2015-05-07 20:13:44 -0700

    Initial Commit

---
+++ testPirateAdventure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testPirateAdventure
+//
+//  Created by Alfred on 5/7/15.
+//  Copyright (c) 2015 maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testPirateAdventure/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testPirateAdventure
+//
+//  Created by Alfred on 5/7/15.
+//  Copyright (c) 2015 maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
